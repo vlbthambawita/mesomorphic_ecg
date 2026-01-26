8a415149aa44f700a1002fb6e2999d0df738a65d
vlbthambawita <vlbthambawita@gmail.com>
2026-01-26 13:33:13 +0000

    updated with lightning

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 #python script_25012026_v1.py --path /work/vajira/DATA/EXG_PTB_XL/physionet/files/ptb-xl/1_0_1 --sampling_rate 100 --epochs 20 --batch_size 64
 
-python script_26012026_v7_gradcam_library_baseline.py \
+python script_25012026_v3_Selected_01_lightning.py \
   --path /work/vajira/DATA/EXG_PTB_XL/physionet/files/ptb-xl/1_0_1 \
   --sampling_rate 500 \
   --epochs 1 \
   --n_pos_viz 10 \
   --n_neg_viz 10 \
   --viz_random \
-  --out_dir runs/mi_vs_norm_imn
+  --out_dir runs/mi_vs_norm_imn \
+  --wandb_project mesormorphic_ecg \
+  --early_stop_patience 10 \
+  --scheduler cosine \
+  --verbose
