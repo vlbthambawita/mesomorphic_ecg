4dd2a31b5c178c794492590d02201c719d210ecc
vlbthambawita <vlbthambawita@gmail.com>
2026-01-27 10:29:44 +0000

    updated script with exp names

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #python script_25012026_v1.py --path /work/vajira/DATA/EXG_PTB_XL/physionet/files/ptb-xl/1_0_1 --sampling_rate 100 --epochs 20 --batch_size 64
 
-python script_25012026_v3_Selected_01_lightning.py \
+python script_25012026_v3_Selected_01_lightning_save_wgen_tokens.py \
   --path /work/vajira/DATA/EXG_PTB_XL/physionet/files/ptb-xl/1_0_1 \
   --sampling_rate 500 \
-  --epochs 100 \
+  --epochs 1 \
   --n_pos_viz 10 \
   --n_neg_viz 10 \
   --viz_random \
@@ -12,4 +12,9 @@ python script_25012026_v3_Selected_01_lightning.py \
   --early_stop_patience 10 \
   --scheduler step \
   --viz_topk \
-  --viz_mc_dropout
+  --viz_mc_dropout \
+  --save_wgen_tokens \
+  --save_wgen_tokens_format all \
+  --save_wgen_tokens_split test \
+  --save_wgen_tokens_max_samples 50 \
+  --exp_name test_run
